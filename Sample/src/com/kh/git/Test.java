ac53e4341c09da44661ac6835a5082f489abea8b
khstudy <39913176+khstudy@users.noreply.github.com>
2018-06-11 23:47:33 +0000

    Reset sample프로젝트

--- Sample/src/com/kh/git/Test.java
+++ Sample/src/com/kh/git/Test.java
@@ -3,11 +3,7 @@ package com.kh.git;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("나를 찍어라");
-		
-		System.out.print("내가 이 프로젝트에 기여해드릴께요");
-		
-		System.out.print("으라차차");		
+		System.out.println("나를 찍어라");		
 	}
 
 }
